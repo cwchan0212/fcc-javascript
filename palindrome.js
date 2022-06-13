eaf60397c451137eaf268076b20053bf11868038
cwzchan <cwchan0212@gmail.com>
2022-06-13 22:51:37 +0800

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,47 @@
+/* FreeCodeCamp > JavaScript Algorithms and Data Structures Projects 
+
+1. Palindrome Checker
+
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+
+We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+
+function palindrome(str) {
+*/ 
+    let strA = str.replace(/[\W_]/g, "").toLowerCase();
+    const arr = strA.split("");
+    //console.log(arr);
+    let l = 0;
+    let r = strA.length - 1;
+    while (l < r) {
+      let m = (l + r) / 2;
+      if (arr[l] != arr[r]) {
+        console.log(false)
+        return false;
+      }
+      l += 1;
+      r -= 1;
+    }
+    console.log(true)
+    return true;
+  }
+  
+  palindrome("race car");
+  palindrome("eye");
+  palindrome("eye"); 
+  palindrome("_eye"); 
+  palindrome("race car"); 
+  palindrome("not a palindrome"); 
+  palindrome("A man, a plan, a canal. Panama"); 
+  palindrome("never odd or even"); 
+  palindrome("nope"); 
+  palindrome("almostomla"); 
+  palindrome("My age is 0, 0 si ega ym."); 
+  palindrome("1 eye for of 1 eye."); 
+  palindrome("0_0 (: /-\ :); 0-0"); 
+  palindrome("five|\_/|four"); 
