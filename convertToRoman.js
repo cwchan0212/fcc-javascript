66ba63c50e063ceed6f3b8e8facb48bbd77f52e1
cwzchan <cwchan0212@gmail.com>
2022-06-14 22:33:21 +0800

    Update convertToRoman.js

--- convertToRoman.js
+++ convertToRoman.js
@@ -1,8 +1,27 @@
-/*
-
+/* FreeCodeCamp > JavaScript Algorithms and Data Structures Projects 
 https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/roman-numeral-converter
+Roman Numeral Converter
+
+Convert the given number into a roman numeral.
+Roman numerals 	Arabic numerals
+M 	1000
+CM 	900
+D 	500
+CD 	400
+C 	100
+XC 	90
+L 	50
+XL 	40
+X 	10
+IX 	9
+V 	5
+IV 	4
+I 	1
+
+All roman numerals answers should be provided in upper-case.
 
 */
+
 function convertToRoman(num) {
 
     const romans = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
